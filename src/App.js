2723de7d6a0af8eb12f1566582a54b73f36d7687
Ike Maldonado <maldonadoike@gmail.com>
2020-08-04 14:55:15 -0700

    Add bootstrap alert that's now functioning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Alert from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 
 function App() {
   return (
